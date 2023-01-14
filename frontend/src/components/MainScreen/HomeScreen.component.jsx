e0af4f08e4a7c4325557cfa7432978765d431219
hadysoufan <hadisoufan74@gmail.com>
2023-01-14 10:57:35 +0200

    Creating the HomeScreen component

---
+++ frontend/src/components/MainScreen/HomeScreen.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../NavBar/Navbar.component';
+
+function HomeScreen() {
+
+  return(
+    <div>
+      <Navbar />
+      
+    </div>
+  )
+
+
+}
+
+export default HomeScreen;
